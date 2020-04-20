909d081081310f3032939bb581bca85a335342e0
ShubhankarChoudhary <shubhankar.choudhary782@gmail.com>
2020-04-20 22:37:42 +0530

    Ignore few files

--- ATM.py
+++ ATM.py
@@ -1,3 +1,4 @@
 print('Has it been pushed, We need to check')
 print('Checked in Github, it worked.')
 print('It worked in Pycharm also, we can proceed with the codes')
+print('Ignoring .idea files')
