2bf72e2c619c9e7745a964777f27a2d04358cd17
ShubhankarChoudhary <shubhankar.choudhary782@gmail.com>
2020-04-22 04:03:24 +0530

    DB Connection added

--- SocketProgram.py
+++ SocketProgram.py
@@ -1,5 +1,13 @@
+import mysql.connector
 import socket
 
+db = mysql.connector.connect(host = '127.0.0.1',user = 'root', passwd = 'Shubhankar@1', database = 'python')
+
+pointer = db.cursor()
+#pointer.execute('show databases')
+print('Connected to database')
+#query="select databases"
+#pointer.execute('show databases')
 
 
 s= socket.socket()
@@ -14,6 +22,9 @@ while True:
     print('Connection Established')
     print('***********************************************************')
     name = ip.recv(1024).decode()
+    pointer2 = db.cursor()
+    #l=list(pointer2.execute("select * from login where Name = 'Shubhankar'"))
+    #print(l)
     print('The name of the Client is ',name,' and Port number is ',port)
     print('***********************************************************')
     ip.send(bytes("Welcome to the world of Sockets",'utf-8'))
